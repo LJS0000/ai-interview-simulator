a8d3d79c9ae4585123c3ec0e4c6e06bcc6a24baa
LJS0000 <jisu2281l@naver.com>
2023-06-02 17:45:57 +0900

    [ERROR] 라우팅 구현
    시뮬레이션 페이지 접근 시 404 Error 발생하여 수정 중

--- index.js
+++
@@ -1,4 +0,0 @@
-import App from './src/app.js';
-
-const app = new App();
-app.initialize();
--- src/app.js
+++ src/app.js
@@ -1,31 +1,16 @@
-import Router from './router/Router.js';
-import Header from './components/Header.js';
-import Qna from './pages/qna.js';
-import Simulation from './pages/simulation.js';
+import Header from './components/header.js';
 
-export default class App {
+class App {
   constructor() {
-    this.router = new Router();
     this.header = new Header();
   }
 
   initialize() {
-    // 라우팅
-    this.router.addRoute('/', () => {
-      const qna = new Qna();
-      qna.render();
-    });
-
-    this.router.addRoute('/simulation', () => {
-      const simulation = new Simulation();
-      simulation.render();
-    });
-
-    this.router.navigate();
-
-    // 헤더
     this.header.addMenuItem('질문하기', '/');
     this.header.addMenuItem('모의면접', '/simulation');
     this.header.render(document.getElementById('header-container'));
   }
 }
+
+const app = new App();
+app.initialize();
