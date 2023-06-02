215ad610b0143abfaee72ea9216d33e189121a0b
LJS0000 <jisu2281l@naver.com>
2023-06-02 21:38:33 +0900

    Revert "[ERROR] 라우팅 구현"
    This reverts commit a8d3d79c9ae4585123c3ec0e4c6e06bcc6a24baa.

--- src/app.js
+++ src/app.js
@@ -1,16 +1,31 @@
-import Header from './components/header.js';
+import Router from './router/Router.js';
+import Header from './components/Header.js';
+import Qna from './pages/qna.js';
+import Simulation from './pages/simulation.js';
 
-class App {
+export default class App {
   constructor() {
+    this.router = new Router();
     this.header = new Header();
   }
 
   initialize() {
+    // 라우팅
+    this.router.addRoute('/', () => {
+      const qna = new Qna();
+      qna.render();
+    });
+
+    this.router.addRoute('/simulation', () => {
+      const simulation = new Simulation();
+      simulation.render();
+    });
+
+    this.router.navigate();
+
+    // 헤더
     this.header.addMenuItem('질문하기', '/');
     this.header.addMenuItem('모의면접', '/simulation');
     this.header.render(document.getElementById('header-container'));
   }
 }
-
-const app = new App();
-app.initialize();
