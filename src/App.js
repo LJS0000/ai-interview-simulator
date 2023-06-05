28d9d05b81159da9d781242728735d7fa16d3493
LJS0000 <jisu2281l@naver.com>
2023-06-05 11:32:19 +0900

    [HOTFIX] 라우터 수정
    1. 배포 시 에러로 라우터 절대 경로로 수정
    2. 404.html 추가

--- src/App.js
+++ src/App.js
@@ -15,19 +15,19 @@ export default class App {
 
   initialize() {
     // 경로와 라우터를 생성합니다.
-    this.router.addRoute('/', () => {
+    this.router.addRoute('/ai-interview-simulator/', () => {
       const qaPage = new QaPage();
       qaPage.render();
     });
-    this.router.addRoute('/simulation', () => {
+    this.router.addRoute('/ai-interview-simulator/simulation', () => {
       const simPage = new SimPage();
       simPage.render();
     });
     this.router.navigate();
 
     // 헤더 네비게이션바
-    this.header.addMenuItem('질문하기', '/');
-    this.header.addMenuItem('모의면접', '/simulation');
+    this.header.addMenuItem('질문하기', '/ai-interview-simulator/');
+    this.header.addMenuItem('모의면접', '/ai-interview-simulator/simulation');
     this.header.render(document.getElementById('header-container'));
 
     // 입력창
