9801d51297cbed66ae3c3bf219c44ebc5ba2b0b3
LJS0000 <jisu2281l@naver.com>
2023-06-12 11:32:01 +0900

    [REFAC] class -> fuction 리팩토링

--- src/App.js
+++ src/App.js
@@ -5,37 +5,37 @@ import SimPage from './pages/SimPage.js';
 import Input from './components/common/Input.js';
 import Footer from './components/common/Footer.js';
 
-export default class App {
-  constructor() {
-    this.router = new Router();
-    this.header = new Header();
-    this.input = new Input();
-    this.footer = new Footer();
-  }
+export default function App() {
+  const router = new Router();
+  const header = new Header();
+  const input = new Input();
+  const footer = new Footer();
 
-  init() {
+  const init = () => {
     const path = window.location.pathname;
 
     // 경로와 라우터를 생성합니다.
-    this.router.addRoute(`/`, () => {
+    router.addRoute(`/`, () => {
       const qaPage = new QaPage();
       qaPage.render();
     });
-    this.router.addRoute(`/simulation`, () => {
+    router.addRoute(`/simulation`, () => {
       const simPage = new SimPage();
       simPage.render();
     });
-    this.router.navigate(path);
+    router.navigate(path);
 
     // 헤더 네비게이션바
-    this.header.addMenuItem('질문하기', `/`);
-    this.header.addMenuItem('모의면접', `/simulation`);
-    this.header.render(document.getElementById('header-container'));
+    header.addMenuItem('질문하기', `/`);
+    header.addMenuItem('모의면접', `/simulation`);
+    header.render(document.getElementById('header-container'));
 
     // 입력창
-    this.input.render(document.getElementById('content-container'));
+    input.render(document.getElementById('content-container'));
 
     // 푸터
-    this.footer.render(document.getElementById('footer-container'));
-  }
+    footer.render(document.getElementById('footer-container'));
+  };
+
+  return init();
 }
