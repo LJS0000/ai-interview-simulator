8c9f8894d55e713a27f16a7aaaa7eee1b61a4fd2
LJS0000 <jisu2281l@naver.com>
2023-06-15 03:29:22 +0900

    [FIX] 시뮬레이션 페이지 렌더링 버그 수정

--- src/pages/SimPage.js
+++ src/pages/SimPage.js
@@ -2,15 +2,24 @@
 import SimStartScreen from '../components/sim/SimStartScreen.js';
 import SimMiddleScreen from '../components/sim/SImMiddleScreen.js';
 
-export default function SimPage(mainContainer) {
+export default function SimPage() {
   const simStartScreen = new SimStartScreen();
   const simMiddleScreen = new SimMiddleScreen();
 
+  const init = () => {
+    const mainContainer = document.querySelector('.main-container');
+    if (mainContainer) {
+      mainContainer.innerHTML = '';
+    }
+  };
+
   this.progress = () => {
+    init();
     simMiddleScreen.render();
   };
 
   this.render = () => {
+    init();
     simStartScreen.render();
   };
 }
