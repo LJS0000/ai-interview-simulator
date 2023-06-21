92ab213a41937a9a639086948c4d35d13a63b086
LJS0000 <jisu2281l@naver.com>
2023-06-21 17:59:22 +0900

    [CHORE] 콘솔로그 삭제

--- src/components/sim/SImMiddleScreen.js
+++ src/components/sim/SImMiddleScreen.js
@@ -8,7 +8,6 @@ import toggleHandler from '../../module/event-handler/toggleHandler.js';
 export default function SimMiddleScreen() {
   const progressbar = new Progressbar();
   const timer = new Timer();
-  const modal = new Modal();
 
   let step = 0;
 
@@ -16,7 +15,7 @@ export default function SimMiddleScreen() {
   const questionText = document.createElement('p');
 
   const init = () => {
-    mainContainer.classList.add('main-container');
+    mainContainer.classList.add('main-sim-container');
     questionText.classList.add('question-text');
 
     questionText.textContent = '면접을 시작하겠습니다.';
@@ -27,21 +26,11 @@ export default function SimMiddleScreen() {
     mainContainer.append(questionText);
   };
 
-  const renderButtons = () => {
-    const answerButton = button('내 답변 보기', 'click', toggleHandler);
-    const restartButton = button('다시하기', 'click', toggleHandler);
-
-    mainContainer.append(answerButton, restartButton);
-  };
-
   this.updateQuestion = (simQuestionList) => {
-    console.log(simQuestionList);
-
     if (step > simQuestionList.length - 1) {
       // 모든 질문을 마친 경우에 대한 처리
       timer.stop();
       questionText.textContent = '수고하셨습니다.';
-      renderButtons();
       this.render();
       return;
     } else {
@@ -54,7 +43,7 @@ export default function SimMiddleScreen() {
     progressbar.updateProgress(step);
     setTimeout(() => {
       this.updateQuestion(simQuestionList);
-    }, 5500); // 5.5초의 지연
+    }, 80500); // 80.5초의 지연
   };
 
   this.render = () => {
